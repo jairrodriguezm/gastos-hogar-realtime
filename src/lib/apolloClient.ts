76d81de3ee89f1bd70651afe29eb2c101e32c7b0
Jair Rodriguez <ing.rodriguezjair@gmail.com>
2025-07-09 08:07:37 -0500

    Added env files

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -3,15 +3,14 @@ import { GraphQLWsLink } from '@apollo/client/link/subscriptions'
 import { createClient } from 'graphql-ws'
 import { getMainDefinition } from '@apollo/client/utilities'
 
-// Reemplaza con tu endpoint de Hasura
-const HASURA_GRAPHQL_ENDPOINT = 'https://wondrous-koala-47.hasura.app/v1/graphql'
-const HASURA_WS_ENDPOINT = 'wss://wondrous-koala-47.hasura.app/v1/graphql'
-const HASURA_ADMIN_SECRET = 'HaGbAAe4DfoSvY5WvtIu4VpJLawRnPYiQnjBYa2HVV5FoJzKHGLfSNhtDolnRFH5' // Opcional si no estás usando autenticación pública
+const HASURA_GRAPHQL_ENDPOINT = import.meta.env.VITE_HASURA_GRAPHQL_ENDPOINT;
+const HASURA_WS_ENDPOINT = import.meta.env.VITE_HASURA_WS_ENDPOINT;
+const HASURA_ADMIN_SECRET = import.meta.env.VITE_HASURA_ADMIN_SECRET;
 
 const httpLink = new HttpLink({
   uri: HASURA_GRAPHQL_ENDPOINT,
   headers: {
-    'x-hasura-admin-secret': HASURA_ADMIN_SECRET, // o puedes usar token de sesión luego
+    'x-hasura-admin-secret': HASURA_ADMIN_SECRET, 
   },
 })
 
