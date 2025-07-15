0c0615c54d4724871d769cd1a648cff2608416b2
Jair Rodriguez <ing.rodriguezjair@gmail.com>
2025-07-15 10:05:08 -0500

    Add PWA support and form styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { ApolloProvider } from '@apollo/client'
 import { apolloClient } from './lib/apolloClient'
+import { registerSW } from 'virtual:pwa-register'
 
 import './styles/global.scss';
+
+registerSW();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ApolloProvider client={apolloClient}>
       <App />
     </ApolloProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,45 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt', 'icons/*'],
+      manifest: {
+        name: 'Gastos del Hogar',
+        short_name: 'Gastos',
+        description: 'Lleva el control mensual de tus gastos del hogar',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#4b139f',
+        icons: [
+          {
+            src: '/icons/pwa-icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/pwa-icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/pwa-icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
+});
