7c185c184e2cb56c43260a25cad4759908e75c0d
Jair Rodriguez <ing.rodriguezjair@gmail.com>
2025-07-16 12:02:32 -0500

    Fix for android and iOS icons

--- vite.config.ts
+++ vite.config.ts
@@ -27,17 +27,25 @@ export default defineConfig({
             src: '/icons/pwa-icon-192.png',
             sizes: '192x192',
             type: 'image/png',
+            purpose: "any"
           },
           {
             src: '/icons/pwa-icon-512.png',
             sizes: '512x512',
             type: 'image/png',
+            purpose: "any"
+          },
+          {
+            src: '/icons/pwa-icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: "maskable"
           },
           {
             src: '/icons/pwa-icon-512.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'any maskable',
+            purpose: "maskable"
           },
         ],
       },
